53a978b438db21443a5747071a55611dfcc3b046
Suthira Owlarn <suthira.owlarn@gmail.com>
2018-09-02 14:51:46 +0200

    add Praer's first baseliner test

--- tests/testthat/test-meanmodel.R
+++ tests/testthat/test-meanmodel.R
@@ -1,17 +1,23 @@
 context("test-meanmodel.R")
 
 test_that("basic mean example works", {
-  dataf <- data_frame(val = 1:100, x = 1:100)
+  dataf <- data.frame(val = 1:100, x = 1:100)
   mod <- mean_model(val ~ x, data=dataf)
-  test_data <- data_frame(x = rep(0, 5))
+  test_data <- data.frame(x = rep(0, 5))
   pred <- predict(mod, test_data)
   expect_equal(pred, rep(50.5, 5))
 })
 
 test_that("another mean example", {
-  dataf <- data_frame(val = 1:50, x = 1:50)
+  dataf <- tibble::data_frame(val = 1:50, x = 1:50)
   mod <- mean_model(val ~ x, data=dataf)
-  test_data <- data_frame(x = 1:10)
+  test_data <- tibble::data_frame(x = 1:10)
   pred <- predict(mod, test_data)
   expect_equal(pred, rep(25.5, 10))
 })
+
+# test_that("error if column to predict doesn't exist", {
+#   expect_error(mean_model(val ~ x, data = data_frame(x = 1:50)))
+# })
+#
+#   expect_error(mean_model(val ~ x, data = data_frame(x = 1:50)) %>% predict(1:10))
