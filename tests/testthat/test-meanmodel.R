e39f91d2f4d30ed25e979fe99333dfba04c461fa
ilsePit <ik heb 5 ideeen>
2018-09-02 15:03:15 +0200

    added a test for negatives, doubles, and NAs

--- tests/testthat/test-meanmodel.R
+++ tests/testthat/test-meanmodel.R
@@ -7,3 +7,37 @@ test_that("basic mean example works", {
   pred <- predict(mod, test_data)
   expect_equal(pred, rep(50.5, 5))
 })
+
+test_that("example with doubles", {
+  dataf <- data_frame(val = as.double(1:100), x = 1:100)
+  mod <- mean_model(val ~ x, data=dataf)
+  test_data <- data_frame(x = rep(0, 5))
+  pred <- predict(mod, test_data)
+  expect_equal(pred, rep(50.5, 5))
+})
+
+test_that("example with negatives", {
+  dataf <- data_frame(val = -99:0, x = 1:100)
+  mod <- mean_model(val ~ x, data=dataf)
+  test_data <- data_frame(x = rep(0, 5))
+  pred <- predict(mod, test_data)
+  expect_equal(pred, rep(-49.5, 5))
+})
+
+test_that("example with negatives that are double", {
+  dataf <- data_frame(val = as.double(-99:0), x = 1:100)
+  mod <- mean_model(val ~ x, data=dataf)
+  test_data <- data_frame(x = rep(0, 5))
+  pred <- predict(mod, test_data)
+  expect_equal(pred, rep(-49.5, 5))
+})
+
+test_that("example with NAs", {
+  dataf <- data_frame(val = 1:100, x = 1:100)
+  dataf[1:5,'val'] <- NA
+  mod <- mean_model(val ~ x, data=dataf)
+  test_data <- data_frame(x = rep(0, 5))
+  pred <- predict(mod, test_data)
+  expect_equal(pred, rep(mean(6:100), 5))
+})
+
