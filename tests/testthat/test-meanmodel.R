0c68d7cc7c87e5dcb297eebecd4d2f4aeab8fb31
vincent d warmerdam <vincentwarmerdam@gmail.com>
2018-09-02 19:21:17 +0200

    automated even more steps

--- tests/testthat/test-meanmodel.R
+++ tests/testthat/test-meanmodel.R
@@ -4,28 +4,19 @@ arr1 <- 1:10
 arr2 <- c(arr1, NA)
 arr3 <- c(-1, 1, 1, 1, 1, 1)
 datasets <- list(arr1, arr2, arr3)
+frame_types <- list(data.frame, data_frame, tibble)
+formulas <- list(y ~ x, y ~ x + z, y ~ .)
 
 test_that("basic mean examples work", {
-  for(arr in list(arr1, arr2, arr3)){
-    for(frame in list(data.frame, data_frame, tibble)){
-      dataf <- frame(arr = arr, x = 1:length(arr))
-      mod <- dummy_regressor(arr ~ x, data=dataf, strategy = "mean")
-      test_data <- data.frame(x = rep(0, 5))
-      expected <- rep(mean(arr, na.rm = TRUE), 5)
-      expect_equal(predict(mod, test_data), expected)
-    }
-  }
-})
-
-test_that("basic mean examples work with ~ . in the formula", {
-  for(arr in list(arr1, arr2, arr3)){
-    for(frame in list(data.frame, data_frame, tibble)){
-      dataf <- frame(arr = arr, x = 1:length(arr))
-      output <- mean(arr, na.rm = TRUE)
-      mod <- dummy_regressor(arr ~ ., data=dataf, strategy = "mean")
-      test_data <- data.frame(x = rep(0, 5))
-      pred <- predict(mod, test_data)
-      expect_equal(pred, rep(output, 5))
+  for(arr in datasets){
+    for(frame in frame_types){
+      for(formula in formulas){
+        dataf <- frame(y = arr, x = 1:length(arr), z = 1)
+        mod <- dummy_regressor(formula, data=dataf, strategy = "mean")
+        test_data <- frame(x = 1:5, z= 1)
+        expected <- rep(mean(arr, na.rm = TRUE), nrow(test_data))
+        expect_equal(predict(mod, test_data), expected)
+      }
     }
   }
 })
