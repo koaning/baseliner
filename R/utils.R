712b918f49c61d756816d8b37f00fafddd480ac2
vincent d warmerdam <vincentwarmerdam@gmail.com>
2018-09-02 20:20:56 +0200

    col_to_predict example fix

--- R/utils.R
+++ R/utils.R
@@ -3,8 +3,8 @@
 #' @param formula that is to be parsed
 #' @return a string with the column name on the lefthand side of formula
 #' @examples
-#' cols_to_explain(y ~ x + z)
-#' cols_to_explain(y ~ .)
+#' col_to_predict(y ~ x + z)
+#' col_to_predict(y ~ .)
 #' @export
 col_to_predict <- function(formula){
   as.character(formula)[2]
